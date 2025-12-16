ea8f686416412d30f9a5fc18d286912958be8d17
Marcus Skjold <marcusskjold@gmail.com>
2025-12-17 00:37:38 +0100

    Add initial working system validator

--- systemvalidator.py
+++ systemvalidator.py
@@ -44,6 +44,7 @@ ARCHITECTURES = [
 ]
 
 DISTRIBUTIONS = [
+    "Rolling",
     "Ardent Apalone",
     "Bouncy Bolson",
     "Kilted Kaiju",
@@ -56,16 +57,43 @@ DISTRIBUTIONS = [
     "Crystal Clemmys"
 ]
 
+# See rmw/rmw/src/qos_string_conversions.c
+QOS = {
+    "history": ["system_default", "keep_last", "keep_all"],
+    "depth": int,
+    "reliability": ["system_default", "best_available", "reliable", "best_effort"],
+    "durability": ["system_default", "best_available", "volatile", "transient_local",],
+    "deadline": int,
+    "lifespan": int,
+    "liveliness": ["automatic", "manual_by_topic", "system_default", "best_available"],
+    "liveliness_lease_duration": int
+}
+
+matches: dict[str, dict[str, list]] = {
+    "services requested": {},
+    "services offered": {},
+    "topics subscribed to": {},
+    "topics published to": {},
+}
+
 objects: dict[str, dict[str, str]] = {
+    "callback": {},
     "external_input": {},
     "external_output": {},
     "executor": {},
     "node": {},
-    "host": {}
+    "host": {},
+    "timer": {},
+    "service": {},
+    "client": {},
+    "variable": {},
+    "publisher": {},
+
+
 }
 
 
-def is_valid_value(typ: str, val: str):
+def is_valid_value(typ: str, val: str) -> list[str]:
     valid_values = {
         "dds": DDS_IMPLEMENTATIONS,
         "distribution": DISTRIBUTIONS,
@@ -75,106 +103,223 @@ def is_valid_value(typ: str, val: str):
     }
 
     if val not in valid_values[typ]:
-        return f"{typ} '{val}' not among {valid_values[typ]}"
+        return [f"{typ} '{val}' not among {valid_values[typ]}"]
+    else:
+        return []
 
 
-def register(obj, typ: str, parent) -> list[str]:
-    name = obj.name
+def register(object_name, object_type: str, parent_name: str) -> list[str]:
+    if (object_name is None) or (object_name == ""):
+        return [f"{object_type} owned by {parent_name} is missing name. Skipping validation of branch."]
+    elif object_name in objects[object_type]:
+        return [f"{object_type} '{object_name}' has multiple owners, or name is not unique among {typ}s. Skipping validation of branch."]
+    else:
+        objects[object_type][object_name] = parent_name
+        return []
+
+
+def verify_registration(object_name: str, object_type: str, parent_name: str, expector: str) -> list[str]:
+    if object_name not in objects[object_type]:
+        return [f"Even though {expector} expected so, {object_type} '{object_name}' is not registered to any parent."]
+    elif parent_name != objects[object_type][object_name]:
+        return [f"Even though {expector} expected so, {object_type} '{object_name}' is not contained within the parent '{parent_name}'"]
+    else:
+        return []
+
+def subset_check(key1: str, key2: str) -> list[str]:
+    keyset1 = matches[key1].keys()
+    keyset2 = matches[key2].keys()
+    if keyset1 <= keyset2:
+        return []
+    else:
+        return [f"Mismatched: Some {key1} are not among {key2}"]
+
+# TODO: Make a check for compatible quality of services between request and offer
+def validate_qos(qos: ros.QualityOfService, parent: str) -> list[str]:
+    feedback = []
+    if qos["history"] not in QOS["history"]:
+        feedback += [f"{parent} has invalid qos history policy"]
+    if qos["depth"] < 0:
+        feedback += [f"{parent} has invalid qos depth policy"]
+    if qos["reliability"] not in QOS["reliability"]:
+        feedback += [f"{parent} has invalid qos reliability policy"]
+    if qos["durability"] not in QOS["durability"]:
+        feedback += [f"{parent} has invalid qos durability policy"]
+    if qos["deadline"] < 0:
+        feedback += [f"{parent} has invalid qos deadline policy"]
+    if qos["lifespan"] < 0:
+        feedback += [f"{parent} has invalid qos lifespan policy"]
+    if qos["liveliness"] not in QOS["liveliness"]:
+        feedback += [f"{parent} has invalid qos liveliness policy"]
+    if qos["liveliness_lease_duration"] < 0:
+        feedback += [f"{parent} has invalid qos liveliness_lease_duration policy"]
+    return feedback
+
+
+def add_match(name: str, container_name: str, typ: str, matches_key: str):
+    """
+    Checks if the name is not empty.
+    Registers the name inside the matches dict.
+    This is done to create a global, nonhierarchical overview of which topics, services, etc.
+    are read from and written to, such that it can be checked at the end if any nodes read from
+    a communication interface that no node writes to.
+    """
     if (name is None) or (name == ""):
-        return [f"{typ} is missing name. Skipping validation of branch. Full {typ}: {print(obj)}"]
-    if obj in objects[typ]:
-        return [f"{typ} '{name}' has multiple owners. Skipping validation of branch."]
-    objects[typ][name] = parent.name
+        return [f"{typ} inside '{container_name}' is missing name."]
+    else:
+        matches[matches_key].setdefault(name, [])
+        matches[matches_key][name].append(container_name)
+        return []
+
+
+
+def validate_client(client: ros.Client, parent: ros.Node) -> list[str]:
+    """
+    A client is well formed if:
+    - It has a name
+    - It is only owned by one node
+    - It has a valid quality of service profile
+    - It names the service it requests
+    """
+    feedback = register(client.name, "client", parent.name)
+    if feedback != []:
+        print(feedback)
+        return feedback
+
+    feedback += validate_qos(client.qos_profile, client.name)
+    feedback += add_match(client.service, client.name, "Service", "services requested")
+
+    return feedback
+
+
+def validate_publisher(publisher: ros.Publisher, parent: ros.Node) -> list[str]:
+    """
+    A publisher is well formed if:
+    - It has a name
+    - It is only owned by one node
+    - It has a valid quality of service profile
+    - It names the topic it publishes to
+    """
+
+    feedback = register(publisher.name, "publisher", parent.name)
+    if feedback != []:
+        return feedback
+
+    feedback += validate_qos(publisher.qos_offered, publisher.name)
+    feedback += add_match(publisher.topic, publisher.name, "topic", "topics published to")
+
+    return feedback
+
+
+def validate_callback(callback: ros.Callback, parent: ros.Node) -> list[str]:
+    """
+    A callback is well formed if:
+    - It has a name
+    - It is only owned by one node
+    - It only uses publishers owned by the parent node
+    - It only reads and writes to variables owned by the parent node
+    - It only outputs to external outputs owned by the parent node
+    - All requests have a valid timeout
+    - All requests refer to a client owned by the parent node
+    - It has a valid wcet
+    """
+    feedback = register(callback.name, "callback", parent.name)
+    if feedback != []:
+        return feedback
+    name = callback.name
+    pname = parent.name
+    for publisher in callback.publishers:
+        feedback += verify_registration(publisher, "publisher", pname, name)
+    for read in callback.read_variables:
+        feedback += verify_registration(read.name, "variable", pname, name)
+    for write in callback.write_variables:
+        feedback += verify_registration(write.name, "variable", pname, name)
+    for output in callback.external_outputs:
+        feedback += verify_registration(output.name, "external_output", pname, name)
+    for request in callback.requests:
+        feedback += verify_registration(request.client.name, "client", pname, name)
+        if request.timeout < 0:
+            feedback += [f"A request of callback '{name}' has a negative timeout."]
+    if callback.wcet < 0:
+        feedback += [f"Callback '{name}' has a negative wcet"]
+
+    return feedback
+
+
+def validate_input(input: ros.ExternalInput, parent: ros.Node) -> list[str]:
+    """
+    An external input is well formed if:
+    - It has a name
+    - It is only owned by one node
+    - It calls a callback that is owned by the same node
+    """
+    feedback = register(input.name, "external_input", parent.name)
+    if feedback != []:
+        return feedback
+
+    feedback += verify_registration(input.callback, "callback", parent.name, input.name)
+
+    return feedback
+
+def validate_subscription(subscription: ros.Subscription, parent: ros.Node) -> list[str]:
+    """
+    A subscription is well formed if:
+    - It has a valid quality of service profile
+    - It names the topic it subscribes to
+    - It calls a callback that is owned by the same node
+    """
+    pname = parent.name
+    feedback = []
+    feedback += validate_qos(subscription.qos_requested, pname)
+    feedback += add_match(subscription.topic, pname, "Topic", "topics subscribed to")
+    feedback += verify_registration(subscription.callback, "callback", pname, pname)
+
+    return feedback
+
+def validate_timer(timer: ros.Timer, parent: ros.Node) -> list[str]:
+    """
+    A subscription is well formed if:
+    - It has a name
+    - It is only owned by one node
+    - It has a valid period
+    - It calls a callback that is owned by the same node
+    """
+    feedback = register(timer.name, "timer", parent.name)
+    if feedback != []:
+        return feedback
+
+    if timer.period < 0:
+        feedback += [f"Timer '{timer.name}' must not have a negative period"]
+
+    feedback += verify_registration(timer.callback, "callback", parent.name, timer.name)
+
+    return feedback
+
+def validate_service(service: ros.Service, parent: ros.node) -> list[str]:
+    """
+    A service is well formed if:
+    - It has a name
+    - It is only owned by one node
+    - It has a valid quality of service profile
+    - It calls a callback that is owned by the same node
+    """
+
+    feedback = register(service.name, "service", parent.name)
+    if feedback != []:
+        return feedback
+
+    feedback += validate_qos(service.qos_requested, service.name)
+    feedback += add_match(service.name, node.name, "service", "services offered")
+
+    feedback += verify_registration(service.callback, "callback", parent.name, service.name)
+    return feedback
+
+
+def validate_action(action: ros.Action, parent: ros.Node) -> list[str]:
+    """
+    TODO
+    """
     return []
-
-
-def is_simple_subscriber(node: ros.Node) -> bool:
-    """
-    A simple subscriber has a single subscription which posts to a unique topic
-    """
-    if len(node.actions) != 0:
-        return False
-    if len(node.clients) != 0:
-        return False
-    if len(node.external_inputs) != 0:
-        return False
-    if len(node.timers) != 0:
-        return False
-    if len(node.variables) != 0:
-        return False
-    if len(node.services) != 0:
-        return False
-    if len(node.publishers) != 1:
-        return False
-    if len(node.subscriptions) != 1:
-        return False
-    if len(node.callbacks) != 1:
-        return False
-
-    pub: ros.Publisher = node.publishers[0]
-    sub: ros.Subscription = node.subscriptions[0]
-    cb: ros.Callback = node.callbacks[0]
-
-    # Check subscriber
-    # TODO: Make this a more meaningful check
-    # TODO: Make a wellformedness error type and a modeltranslation error type
-    sub.na
-    if sub.buffer < 1:
-        raise WellformednessError(f"""
-        Subscriber of {node.name} has a too small buffer
-        """)
-    if sub.callback != cb.name:
-        raise WellformednessError(f"""
-        Node '{node.name}' is not well-formed.
-        Subscription does not call callback '{cb.name}'
-        """)
-
-    # Check publisher
-    if pub.topic in topicpublishers:
-        raise TranslationError(f"""
-        All nodes are assumed to publish to unique topics,
-        however both {node.name} and {topicpublishers[pub.topic]} publish to
-        topic {pub.topic}
-        """)
-    if pub.buffer < 1:
-        raise TranslationError(f"""
-        Buffers are assumed to be large enough to avoid overflow,
-        publisher of {node.name} has a too small buffer
-        """)
-
-    # Check callback
-    if cb.name is None: raise WellformednessError(f"Callback of node '{node.name}' has no name")
-    if len(cb.calls) != 0:
-        return False
-    if len(cb.write_variables) != 0:
-        raise WellformednessError( f"Callback '{cb.name}' writes to variables outside of parent node")
-    if len(cb.requests) != 0:
-        raise WellformednessError( f"Callback '{cb.name}' has requests, yet parent node has no clients")
-    if len(cb.external_outputs) != 0:
-        return False
-    if len(cb.read_variables) != 0:
-        raise WellformednessError( f"Callback '{cb.name}' reads from variables outside of parent node")
-    if cb.wcet is None:
-        raise WellformednessError(f"Callback '{cb.name}' has now wcet")
-    if len(cb.publishers) != 1:
-        return False
-    if cb.publishers[0] != pub.name:
-        raise WellformednessError(f"""
-        Node '{node.name}' is not well-formed.
-        Callback '{cb.name}' does not publish to '{pub.name}'
-        """)
-
-    if sub.topic not in topicsubscribers:
-        topicsubscribers[sub.topic] = []
-    topicsubscribers[sub.topic].append(node.name)
-    topicpublishers[pub.topic] = node.name
-
-    return True
-
-
-"""
-Callbacks own requests
-"""
 
 
 def validate_node(node: ros.Node, parent: ros.Executor) -> list[str]:
@@ -182,44 +327,69 @@ def validate_node(node: ros.Node, parent: ros.Executor) -> list[str]:
     A node is well formed if:
     - It has a name
     - It is only owned by one executor
+    - It has at least one trigger
+    - It has at least one callback
+    - All variables have names
+    - All variables are only owned by this node
+    - All external outputs have names
+    - All external outputs are only owned by this callback
 
-    - TODO: actions are valid
-    - All external inputs are well formed
-    - All variables are well formed
+    - All contained items are well formed:
+        - External inputs
+        - Services
+        - Subscriptions
+        - Actions
+        - Timers
+        - Actions (TODO)
+        - Clients
+        - Publishers
+        - Callbacks
+
+    - All callbacks only call callbacks that are also owned by this node
     """
-    feedback = register(node, "node", parent)
-    if feedback is not []:
+    feedback = register(node.name, "node", parent.name)
+    if feedback != []:
         return feedback
 
-    for input in system.external_inputs:
-        feedback.extend(register(input, "external_input"))
-
-    for output in system.external_outputs:
-        feedback.extend(register(input, "external_outputs"))
-
-
-    # Unvalidated
-
-    # triggers
-    node.external_inputs
-    node.services
-    node.subscriptions
-    node.timers
-
     # outputs
-    node.clients
-    node.publishers
+    for client in node.clients:
+        feedback += validate_client(client, node)
+    for publisher in node.publishers:
+        feedback += validate_publisher(publisher, node)
 
     # internal
-    node.callbacks
     for variable in node.variables:
-        validate_variable(variable)
+        feedback += register(variable.name, "variable", node.name)
+    for output in node.external_outputs:
+        feedback += register(output.name, "external_output", node.name)
+    if len(node.callbacks) < 1:
+        feedback += f"Node '{node.name}' must have at least one callback"
+    for callback in node.callbacks:
+        feedback += validate_callback(callback, node)
 
-    # TODO: Add support for actions
-    actions = node.actions
+    total_triggers = 0
+    for input in node.external_inputs:
+        feedback += validate_input(input, node)
+        total_triggers += 1
+    for subscription in node.subscriptions:
+        feedback += validate_subscription(subscription, node)
+        total_triggers += 1
+    for timer in node.timers:
+        feedback += validate_timer(timer, node)
+        total_triggers += 1
+    for service in node.services:
+        feedback += validate_service(service, node)
+        total_triggers += 1
+    for action in node.actions:
+        validate_action(action, node)  # TODO: Add support for actions
+        total_triggers += 1
+    if total_triggers < 1:
+        feedback += f"Node '{node.name}' must have at least one trigger"
 
+    for callback in node.callbacks:
+        for called_name in callback.calls:
+            feedback += verify_registration(called_name, "callback", node.name, callback.name)
     return feedback
-
 
 
 def validate_executor(executor: ros.Executor, parent: ros.Host) -> list[str]:
@@ -232,19 +402,18 @@ def validate_executor(executor: ros.Executor, parent: ros.Host) -> list[str]:
     - It has at least one node
     - All nodes are well formed
     """
-    feedback = register(executor, "executor", parent)
-    if feedback is not []:
+    feedback = register(executor.name, "executor", parent.name)
+    if feedback != []:
         return feedback
 
-    feedback.append(is_valid_value("distribution", executor.ros_distribution))
-    feedback.append(is_valid_value("executor", executor.implementation))
+    feedback += is_valid_value("distribution", executor.ros_distribution)
+    feedback += is_valid_value("executor", executor.implementation)
 
-    nodes = executor.nodes
-    if len(nodes) < 1:
-        feedback.append(f"Executor '{executor.name}' must have at least one node")
+    if len(executor.nodes) < 1:
+        feedback += [f"Executor '{executor.name}' must have at least one node"]
 
-    for node in nodes:
-        feedback.extend(validate_node(node, executor))
+    for node in executor.nodes:
+        feedback += validate_node(node, executor)
     return feedback
 
 
@@ -257,19 +426,19 @@ def validate_host(host: ros.Host, parent: ros.System) -> list[str]:
     - It has at least one executor
     - All executors are well formed
     """
-    feedback = register(host, "host", parent)
-    if feedback is not []:
+    feedback = register(host.name, "host", parent.name)
+    if feedback != []:
         return feedback
 
-    feedback.append(is_valid_value("os", host.operating_system))
-    feedback.append(is_valid_value("architecture", host.architecture))
+    feedback += is_valid_value("os", host.operating_system)
+    feedback += is_valid_value("architecture", host.architecture)
 
     executors = host.executors
     if len(executors) < 1:
-        feedback.append(f"Host '{host.name}' must have at least one executor")
+        feedback += [f"Host '{host.name}' must have at least one executor"]
 
     for executor in executors:
-        feedback.extend(validate_executor(executor, host))
+        feedback += validate_executor(executor, host)
     return feedback
 
 
@@ -280,20 +449,23 @@ def validate_system(system: ros.System) -> list[str]:
     - It has a valid dds
     - It has at least one host
     - All hosts are well formed
+    - There is a server offering each service that a client requests
+    - There is a publisher to each topic that a subscriber subscribes to
     """
     feedback = []
 
     if (system.name is None) or (system.name == ""):
-        feedback.append("System must have a name")
+        feedback += ["System must have a name"]
 
-    feedback.append(is_valid_value("dds", system.dds_implementation))
+    feedback += is_valid_value("dds", system.dds_implementation)
 
     hosts = system.hosts
     if len(hosts) < 1:
-        feedback.append("System must have at least one host")
-
+        feedback += ["System must have at least one host"]
     for host in hosts:
-        feedback.extend(validate_host(host), system)
+        feedback += validate_host(host, system)
+    feedback += subset_check("services requested", "services offered")
+    feedback += subset_check("topics subscribed to", "topics published to")
 
     if feedback == []:
         return ["System is well formed"]
