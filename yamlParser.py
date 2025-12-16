d70aca7405ea95e61aa026b262a83ea524dbede4
Andreas Riget Bagge <andbag@live.dk>
2025-12-16 10:34:03 +0100

    Update .gitignore and new parserlib using ruamel.yaml now, as it seems to avoid some bugs from pyyaml such as handling of duplicate keys

--- yamlParser.py
+++ yamlParser.py
@@ -1,13 +1,20 @@
 import ros2system as ros
-import yaml #external library for parsing yaml into object (defacto standard, it seems)
+##old version (handles order of dict improperly and overwrites in case of duplicates)
+#import yaml #external library for parsing yaml into object (defacto standard, it seems)
+from ruamel.yaml import YAML
+from pprint import pprint
+
 
 #load the yaml-file
-with open('example.yaml','r') as file:
-    yaml_object = yaml.safe_load(file)
+with open('example_simpler.yaml','r') as file:
+    #yaml_object = yaml.safe_load(file)
+    yaml=YAML(typ='safe')   # default, if not specfied, is 'rt' (round-trip)
+    yaml_object = yaml.load(file)
     ##debug ex
     #print(yaml_object['System']['hosts'][0]['executors'][0]['implementation'])
     if(len(yaml_object)!=1 or not ('System' in yaml_object)):
         raise SyntaxError("file must have single outer-key 'System'")
-    print(yaml_object)
+    pprint(yaml_object, sort_dicts=False)
 
-#TODO: should probably use ruamel.yaml instead to avoid duplicate key errors etc.? (and preserving order of arguments) 
+#TODO: check that argument order is preserved
+    #check that using C-version of ruamel is okay(see website)
