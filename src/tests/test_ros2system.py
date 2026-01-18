ad89ab2a5490d69498c6fa7c49bb0af58fa6ec1f
Marcus Skjold <marcusskjold@gmail.com>
2026-01-18 11:24:22 +0100

    Add test of default qos initialization

--- src/tests/test_ros2system.py
+++ src/tests/test_ros2system.py
@@ -1,10 +1,13 @@
-import ros2model.ros2system as ros
-import pytest
+from roserer.ros2system import *
 
 
-def func(x):
-    return x + 1
-
-
-def test_answer():
-    assert func(3) == 5
+def test_qualityofservice_defaultinit_matches_rosdefaults() -> None:
+    qos = QualityOfService()
+    assert qos.history == "keep_last"
+    assert qos.depth == 10
+    assert qos.reliability == "reliable"
+    assert qos.durability == "volatile"
+    assert qos.deadline == 0
+    assert qos.lifespan == 0
+    assert qos.liveliness == "system_default"
+    assert qos.liveliness_lease_duration == 0
