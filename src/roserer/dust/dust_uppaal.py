f572a174e873c28d9905c2633a50a9c7df5a27ed
Andreas Riget Bagge <andbag@live.dk>
2026-01-22 13:05:29 +0100

    first full dustToUppaal
    Now possible to define system in dust-python-object and initialize, query and parse query-results in UPPAAL. Have not made parser for query 5, as it depends on use-case what you need from trace. This can be added at will

--- src/roserer/dust/dust_uppaal.py
+++ src/roserer/dust/dust_uppaal.py
@@ -7,18 +7,15 @@
 # # damages, or other liability, whether in an action of contract, tort, or otherwise, arising from,
 # # out of, or in connection with the software or the use or other dealings in the software.
 
-
 # ## Provides functions to perform various UPPAAL tasks.
 
 import subprocess
-import itertools
-import re
 import roserer.verifyta_resolver
 
 # Class containing static functions for interacting with UPPAAL
 class UPPAAL():
     def run_uppaal(
-        modelfile: str,
+        modelfile: str, 
         queryfile: str,
         extra_args: list[str] | None = None
     ) -> str:
@@ -27,8 +24,6 @@ class UPPAAL():
         try:
             output = subprocess.check_output(
                 [roserer.verifyta_resolver.find_verifyta()] + extra_args + [modelfile, queryfile], text=True)
-                # ["./verifyta"] + extra_args + [modelfile, queryfile], text=True)
-
             return output
         except Exception:
             # We assume that any error is due to overflow in scheduling queue.
@@ -36,15 +31,14 @@ class UPPAAL():
             10/0
             return "Overflow"
 
-    # TODO: try to loop here to create dict of results or something like that -> manage more checkables
     # query 2) from paper
-    def buffer_overflow(modelfile, checkables):
+    def buffer_overflow(modelfile : str, checkables : list[str]) -> dict:
         queryfile = modelfile + '.q'
         UPPAAL.write_buffer_overflow_query(queryfile, checkables)
         output = UPPAAL.run_uppaal(modelfile, queryfile)
-        return UPPAAL.parse_buffer_overflow_query(output)
+        return UPPAAL.parse_buffer_overflow_query(output, checkables)
 
-    def write_buffer_overflow_query(queryfile, checkables) -> str:
+    def write_buffer_overflow_query(queryfile : str, checkables : list[str]) -> str:
         fout = open(queryfile, 'w')
         q = ""
         for checkable in checkables:
@@ -52,29 +46,31 @@ class UPPAAL():
         fout.write(q)
         return q
 
-    def parse_buffer_overflow_query(output):
-        satisfied = None
+    def parse_buffer_overflow_query(output : str, checkables : list[str]) -> dict:
         lines = output.split("\n")
         idx = 0
-        while "Verifying formula" not in lines[idx]:
+        results = {}
+        for checkable in checkables:
+            while "Verifying formula" not in lines[idx]:
+                idx += 1
+            verdict = lines[idx+1].strip() # Formula is/NOT satisfied
+            if "Formula is NOT satisfied" in verdict:
+                results[checkable] = False
+            elif "Formula is satisfied" in verdict:
+                results[checkable] = True
             idx += 1
-        l2 = lines[idx+1].strip() # Formula is/NOT satisfied
-        if "Formula is NOT satisfied" in l2:
-            satisfied = False
-        elif "Formula is satisfied" in l2:
-            satisfied = True
-        return satisfied
+        return results
 
     # query 3) from paper
-    def max_buffer_size(modelfile, checkables):
+    def max_buffer_size(modelfile : str, checkables : list[str]):
         queryfile = modelfile + '.q'
         UPPAAL.write_max_buffer_size_query(queryfile, checkables)
         output = UPPAAL.run_uppaal(modelfile, queryfile)
-        return UPPAAL.parse_max_buffer_size_query(output)
+        return UPPAAL.parse_max_buffer_size_query(output, checkables)
         ## debug
         #return output
 
-    def write_max_buffer_size_query(queryfile, checkables):
+    def write_max_buffer_size_query(queryfile : str, checkables : list[str]):
         fout = open(queryfile, 'w')
         q = ""
         for checkable in checkables:
@@ -82,17 +78,17 @@ class UPPAAL():
         fout.write(q)
         return q
 
-    def parse_max_buffer_size_query(output):
-        return UPPAAL.parse_sup_query(output)
+    def parse_max_buffer_size_query(output : str, checkables : list[str]):
+        return UPPAAL.parse_sup_query(output, checkables)
 
     # query 4) from paper
-    def max_latency(modelfile, checkables):
+    def max_latency(modelfile : str, checkables : list[str]):
         queryfile = modelfile + '.q'
         UPPAAL.write_max_latency_query(queryfile, checkables)
         output = UPPAAL.run_uppaal(modelfile, queryfile)
-        return UPPAAL.parse_max_latency_query(output)
+        return UPPAAL.parse_max_latency_query(output, checkables)
 
-    def write_max_latency_query(queryfile, checkables):
+    def write_max_latency_query(queryfile : str, checkables : list[str]):
         fout = open(queryfile, 'w')
         q = ""
         for checkable in checkables:
@@ -100,200 +96,42 @@ class UPPAAL():
         fout.write(q)
         return q
 
-    #TODO: This could probably return a mapping from checkable to result, which could then be used in query 5!!
-    def parse_max_latency_query(output):
-        return UPPAAL.parse_sup_query(output)
+    def parse_max_latency_query(output : str, checkables : list[str]):
+        return UPPAAL.parse_sup_query(output, checkables)
     
     # query 5) from paper
-    def max_latency_trace(modelfile, checkables):
+    def max_latency_trace(modelfile : str, checkables : list[str], max_latencies : dict = None):
+        # get max latency for each checkable, if not provided
+        if max_latencies is None:
+            max_latencies = UPPAAL.max_latency(modelfile, checkables)
         queryfile = modelfile + '.q'
-        UPPAAL.write_max_latency_trace_query(queryfile, checkables)
-        output = UPPAAL.run_uppaal(modelfile, queryfile)
-        return UPPAAL.parse_max_latency_trace_query(output)
+        UPPAAL.write_max_latency_trace_query(queryfile, max_latencies)
+        ## returns raw trace -> needs to be parsed depending on use-case
+        output = ""
+        #output = UPPAAL.run_uppaal(modelfile, queryfile, ['-t', '1'])
+        #return UPPAAL.parse_max_latency_trace_query(output, checkables)
+        ##debug
+        return output
 
-    #TODO: this requires result from query 4 on a per checkable basis
-        # -> figure out smart way to implement this
-    def write_max_latency_trace_query(queryfile, checkables):
-        return ""
+    def write_max_latency_trace_query(queryfile : str, max_latencies : dict):
+        fout = open(queryfile, 'w')
+        q = ""
+        for checkable, max_latency in max_latencies.items():
+            q += f"E<> {checkable}.ExecutionFinished and {checkable}.relTim[{checkable}.relcnt-1]=={max_latency}\n"
+        fout.write(q)
+        return q
 
+    # TODO: can be implemented depending on use-case
     def parse_max_latency_trace_query():
         return ""
 
-
-    def parse_sup_query(output):
+    def parse_sup_query(output, checkables):
         lines = output.split("\n")
-        for l in lines:
-            if "sup:" in l:
-                return int(l[5:-1])
-
-#     def write_sup_query(queryfile):
-#         fout = open(queryfile, 'w')
-#         fout.write("sup{monitor.measure}: monitor.x[lm]\n")
-#         fout.close()
-
-
-#     def write_measure_load_query(queryfile, load_threshold, percentage, upper_limit=10000):
-#         fout = open(queryfile, 'w')
-#         formula = "Pr [<=" + str(upper_limit) + "] (<> monitor.measure && monitor.x[lm] >= " + str(load_threshold) + ") <= " + str(percentage)
-#         fout.write(formula + "\n")
-#         fout.close()
-#         return formula
-
-#     def write_random_trace_query(queryfile, upper_limit):
-#         fout = open(queryfile, 'w')
-#         fout.write("E<> global == " + str(upper_limit) + "\n")
-#         fout.close()
-
-
-
-#     # Also retursn final data...
-#     def parse_random_trace_query(output):
-
-#         #
-#         #  Current idea:
-#         #  - We register whenever a task starts executing (host.takenext->host.executing)
-#         #  - We register whenever a task finished executing (host.executing->host.done)
-#         #       then we also store which jobs was done (host.job)
-#         #  - Also register when data-sources are firing
-#         def parse_state(s, v, t):
-#             actions = []
-#             # string = ""
-#             string = "<<<STATE>>>\n"
-#             string += s[2:-2] + "\n"
-#             for l in s[2:-2].split(" "):
-#                 string += "  " + l + "\n"
-#             string += str(v) + "\n"
-#             string += str(t) + "\n"
-
-#             values = {}
-#             for vv in v.split(" "):
-#                 split = vv.strip().split("=")
-#                 values[split[0]] = split[1]
-
-#             for tt in t:
-#                 fire_pattern = r"(\w+)\.monitored_fire->\1\.wait"
-#                 match = re.search(fire_pattern, tt)
-#                 if match:
-#                     source_id = values[match.group(1) + ".id"]
-#                     actions.append((values['global'], "FIRE", source_id, values["PAYLOAD"]))
-
-#                 fire_pattern = r"(\w+)\.fire->\1\.wait"
-#                 match = re.search(fire_pattern, tt)
-#                 if match:
-#                     source_id = values[match.group(1) + ".id"]
-#                     actions.append((values['global'], "FIRE", source_id, "-1")) # Here -1 is hard-coded
-
-
-#                 window_pattern = r"check->host\.takenext"
-#                 match = re.search(window_pattern, tt)
-#                 if match:
-#                     actions.append((values['global'], "WINDOW", "", ""))
-
-
-#                 start_pattern = r"host\.takenext->host\.executing"
-#                 match = re.search(start_pattern, tt)
-#                 if match:
-#                     actions.append((values['global'], "START", "", ""))
-
-
-#                 done_pattern = r"host\.executing->host\.done"
-#                 match = re.search(done_pattern, tt)
-#                 if match:
-#                     actions.append((values['global'], "DONE", values['host.job'], values['host.data']))
-
-#                 measure_pattern = r"monitor\.i->monitor\.measure"
-#                 match = re.search(measure_pattern, tt)
-#                 if match:
-#                     lm = values["lm"]
-#                     payload = values["monitor_payload[" + values["lm"] + "]"]
-#                     actions.append((values['global'], "MONITOR", payload, ''))
-
-#             return actions
-
-#         i = 0
-#         lines = output.split("\n")
-#         actions = []
-#         while i < len(lines):
-#             l = lines[i].strip()
-#             if "State:" == l:
-#                 state_line = lines[i+1]
-#                 value_line = lines[i+2]
-#                 transition_lines = []
-#                 j = i+5
-#                 while j < len(lines) and not lines[j].strip() == "":
-#                     transition_lines.append(lines[j].strip())
-#                     j += 1
-
-#                 new_actions = parse_state(state_line, value_line, transition_lines)
-#                 actions += new_actions
-
-#                 i = j
-#             i += 1
-
-#         return actions
-
-#     def check_overload(output):
-#         for l in output.split("\n"):
-#             if "Overflow" in l: # todo: 20 hard-coded here
-#                 return True
-
-#         return False
-
-#     def parse_load_query(output):
-#         #print(output)
-#         satisfied = None
-#         lines = output.split("\n")
-#         idx = 0
-#         while "Verifying formula" not in lines[idx]:
-#             idx += 1
-
-#         #print("!!!", lines[idx])
-#         l1 = lines[idx] # "Verifying formula 1 ..."
-#         l2 = lines[idx+1].strip() # Formula is/NOT satisfied
-#         l3 = lines[idx+2] # (x/y runs) H1: ...
-#         l4 = lines[idx+3] # with confidence
-#         #
-#         if "Formula is NOT satisfied" in l2:
-#             satisfied = False
-#         elif "Formula is satisfied" in l2:
-#             satisfied = True
-
-
-#         return satisfied
-
-
-#     # Finds upper bound on reaction time
-#     def sup(modelfile):
-#         queryfile = modelfile + ".q"
-#         UPPAAL.write_sup_query(queryfile)
-#         output = UPPAAL.run_uppaal(modelfile, queryfile)
-#         return UPPAAL.parse_sup_query(output)
-
-#     # Gets a trace satisfying query
-#     def get_trace(modelfile, query):
-#         queryfile = modelfile + ".q"
-#         f = open(queryfile, 'w')
-#         f.write(query + '\n')
-#         f.close()
-#         output = UPPAAL.run_uppaal(modelfile, queryfile, ['-t', '1'])
-#         return UPPAAL.parse_random_trace_query(output)
-
-
-#     # Gets a random trace until time upper_limit
-#     def random_trace(modelfile, upper_limit):
-#         queryfile = modelfile + ".q"
-#         UPPAAL.write_random_trace_query(queryfile, upper_limit)
-#         output = UPPAAL.run_uppaal(modelfile, queryfile, ['-t', '1'])
-#         return UPPAAL.parse_random_trace_query(output)
-
-
-#     # Used for use case, checks if the system is under load_threshold, with percentage chance
-#     def measure_load(modelfile, load_threshold, percentage, upper_limit):
-#         queryfile = modelfile + ".q"
-#         formula = UPPAAL.write_measure_load_query(queryfile, load_threshold, percentage, upper_limit)
-#         output = UPPAAL.run_uppaal(modelfile, queryfile)
-#         if UPPAAL.check_overload(output):
-#             return formula, "Overload"
-
-#         data = UPPAAL.parse_load_query(output)
-#         return formula, data
+        results = {}
+        idx = 0
+        for checkable in checkables:
+            while "sup:" not in lines[idx]:
+                idx += 1
+            results[checkable] = int(lines[idx][5:-1])
+            idx += 1
+        return results
