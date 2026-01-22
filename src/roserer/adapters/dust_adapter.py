eaf7ee16fd01e8d9dabc5570537db51eb99b7207
Andreas Riget Bagge <andbag@live.dk>
2026-01-22 17:20:23 +0100

    more invariants listed

--- src/roserer/adapters/dust_adapter.py
+++ src/roserer/adapters/dust_adapter.py
@@ -8,11 +8,25 @@ import roserer.systemvalidator as validator
 
 ############################----VALIDATION----############################
 
+####All templates
+# id's must be unique (with respect to other callbacks of same type under same executor, and between executors)
+
 
 ####All callbacks
 # INVARIANT: Timer-buffer size 1?
+# is the type-parameter within bounds (0 - 3)
+# two last arrays should (probably/preferably) be all 0 when no subscribers
+# the release-times for subscribers should be (weakly?) increasing
+# all referenced id's should correspond to something (maybe checked in validator already?)
+# if Timer-type, then buffersize should be 1
 
+
+#### datacallback
+# can not be a timer-callback?
 
 
 ####Sporadic callback
 # INVARIANT: length must correspond to non-zero values in releases-array (except if first is zero?)
+
+####Periodic callback
+# invariant: period should be less than execution-time -> else overflow is more or less guaranteed?
