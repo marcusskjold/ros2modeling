850b32822d4bf1a80fb37d0a6216841cc7ddcb74
Andreas Riget Bagge <andbag@live.dk>
2026-01-26 17:29:54 +0100

    initial transformations
    initial code structure for the dust_adapter

--- src/roserer/adapters/dust_adapter.py
+++ src/roserer/adapters/dust_adapter.py
@@ -1,8 +1,7 @@
 import roserer.dust.dust_system as ds
 import roserer.ros2system as ros
 import roserer.systemvalidator as validator
-
-
+import itertools
 
 
 
@@ -10,6 +9,9 @@ import roserer.systemvalidator as validator
 
 ####All templates
 # id's must be unique (with respect to other callbacks of same type under same executor, and between executors)
+# the system should only use distributions that has the relevant executors (not newer than Humble?)
+# read/write variables can not be used -> should prob generate warning, at least.
+# calls doesn't make sense either. (maybe not even external i/o)
 
 
 ####All callbacks
@@ -19,6 +21,8 @@ import roserer.systemvalidator as validator
 # the release-times for subscribers should be (weakly?) increasing
 # all referenced id's should correspond to something (maybe checked in validator already?)
 # if Timer-type, then buffersize should be 1
+# must have a wcet?
+# a callback must be associated to exactly 1 executor
 
 
 #### datacallback
@@ -30,3 +34,62 @@ import roserer.systemvalidator as validator
 
 ####Periodic callback
 # invariant: period should be less than execution-time -> else overflow is more or less guaranteed?
+
+
+
+################ TRANSLATION ################
+
+VALID_ROS_DISTRIBUTIONS = {
+    "V2" : 
+    [
+        "Iron", # TODO: is this the case?
+        "Iron Irwini", 
+        "Humble",
+        "Humble Hawksbill"
+        "Galactic",
+        "Galactic Geochelone",
+        "Foxy",
+        "Foxy Fitzroy",
+        "Eloquent",
+        "Eloquent Elusor",
+    ],
+    "V1" :
+    [
+        "Dashing",
+        "Crystal",
+        "Bouncy",
+        "Ardent",
+        "Dashing Diademata",
+        "Crystal Clemmys",
+        "Bouncy Bolson",
+        "Ardent Apalone",
+    ]
+}
+
+
+#Should we discern between nodes under same executor?
+# TODO: find smartest way to discern between callback-types in meaningful way?
+def map_node(out: ds.System, executor: ros.Executor, node: ros.Node):
+    return ""
+
+# generate id for Executors
+ex_id = itertools.count()
+def next_exec():
+  return next(ex_id)
+
+
+def map_executor(out: ds.System, executor: ros.Executor) -> None:
+    if executor.ros_distribution in VALID_ROS_DISTRIBUTIONS["V2"]:
+        # TODO: specify executor-id properly (maybe with env-dict?)
+        out.add_executor_v2(next_exec(), -1)
+    else:
+        out.add_executor_v1(next_exec(), -1)
+    for node in executor:
+        map_node(out, executor, node)
+
+def map_system(system: ros.System) -> ds.System:
+    out = ds.System(system.name)
+    for host in system.hosts:
+        for executor in host.executors:
+            map_executor(out, executor)
+    return out
