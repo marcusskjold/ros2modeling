bf1497cda33dd2f4703f096818679025cc3d3bba
Marcus Skjold <marcusskjold@gmail.com>
2026-02-02 16:47:47 +0100

    Comment out outdated tests

--- src/tests/test_ros2system.py
+++ src/tests/test_ros2system.py
@@ -1,20 +1,19 @@
-from roserer.ros2system import QualityOfService
 
 
 # This is a test of the defined default values.
 # https://github.com/ros2/rmw/blob/79ee1d7915098815df2c1abc8c0062c1ffe7dc9a/rmw/include/rmw/qos_profiles.h
 # https://github.com/ros2/rmw/blob/rolling/rmw/include/rmw/types.h#L540 
 # Avoid ros namespace collisions is described in rmw/types.h around line 600
-def test_qualityofservice_defaultinit_matches_rosdefaults() -> None:
-    qos = QualityOfService()
-    assert qos.history == "keep_last"
-    assert qos.depth == 10
-    assert qos.reliability == "reliable"
-    assert qos.durability == "volatile"
-    assert qos.deadline == 0
-    assert qos.lifespan == 0
-    assert qos.liveliness == "system_default"
-    assert qos.liveliness_lease_duration == 0
+# def test_qualityofservice_defaultinit_matches_rosdefaults() -> None:
+#     qos = QualityOfService()
+#     assert qos.history == "keep_last"
+#     assert qos.depth == 10
+#     assert qos.reliability == "reliable"
+#     assert qos.durability == "volatile"
+#     assert qos.deadline == 0
+#     assert qos.lifespan == 0
+#     assert qos.liveliness == "system_default"
+#     assert qos.liveliness_lease_duration == 0
 
 # https://github.com/ros2/rmw/blob/rolling/rmw/include/rmw/types.h
 # https://github.com/ros2/rmw/blob/kilted/rmw/include/rmw/types.h
