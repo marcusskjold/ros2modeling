696c1c698f028cb097e2044a64feeb736795dc9f
Andreas Riget Bagge <anrb@itu.dk>
2026-03-02 10:32:15 +0100

    more tests for calls-validation
    two tests added for validation of calls in system_validator. They check correct validation and registration of callbacks that have nested calls. Yaml-files has been provided as input for the test-cases, and the tests has indirectly also shown results as expected for the rest of the tool-chain up until the validations

--- src/tests/test_systemvalidator.py
+++ src/tests/test_systemvalidator.py
@@ -1,4 +1,6 @@
 from roserer.systemvalidator import is_valid_value
+import roserer.yamlParser as yparser
+import roserer.systemvalidator as sv
 
 
 def test_is_valid_value_dds_positive() -> None:
@@ -32,3 +34,36 @@ def test_is_valid_value_executor_positive() -> None:
     assert is_valid_value("executor", "SingleThreadedExecutor") == []
     assert is_valid_value("executor", "MultiThreadedExecutor") == []
     assert is_valid_value("executor", "EventsExecutor") == []
+
+def test_validate_calls_detects_cycle() -> None:
+    """
+    Tests that a callback-function doesn't call itself infinitely.
+    """
+    test_sys = yparser.parse_yaml("src/tests/input/system_validator/test_validate_calls_detects_cycle.yaml")
+    val_result = sv.validate_system(test_sys)     
+    assert val_result.errors[0] == "The chain of calls, ['cb', 'nested_cb', 'nested_nested_cb'], is circular. Ensure acyclic chain of calls between callbacks."
+
+def test_validate_calls_registers_root() -> None:
+    """
+    Tests that root_node is registered as sending messages to same topics/services as chained cb's
+    in 'calls'
+    """
+    test_sys = yparser.parse_yaml("src/tests/input/system_validator/test_validate_calls_registers_root.yaml")
+    val_result = sv.validate_system(test_sys)
+    expected_interf = {
+        "services requested" :
+            {"service_1" : ["nested_nested_cb", "cb", "nested_cb"]},
+        "services offered" : 
+            {"service_1" : ["service_cb"]},
+        "services received from":
+            {"service_1" : ["response_cb"]},
+        "topics published to" :
+            {"topic_1" : ["nested_cb", "cb"]},
+        "topics subscribed to" :
+            {"topic_1" : ["sub_cb"]},
+        "variables read from" :
+            {},
+        "variables written to" : 
+            {}
+    }
+    assert val_result.interfaces == expected_interf
