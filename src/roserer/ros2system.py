1a7ad254c1a7931f9f6814106ded4f2e2adf88c8
Marcus Skjold <marcusskjold@gmail.com>
2026-03-08 22:06:12 +0100

    Add todo questions

--- src/roserer/ros2system.py
+++ src/roserer/ros2system.py
@@ -168,6 +168,7 @@ class Subscription():
     topic: Topic
     callback: str
     qos: QoS
+    # TODO: Can we convert "wall times" into external input?
     wall_times: list[TimeUnit] | None
 
 @dataclass
@@ -175,6 +176,7 @@ class Service():
     name: str
     callback: str
     qos: QoS
+    # TODO: Can we convert "wall times" into external input?
     wall_times: list[TimeUnit] | None
 
 @dataclass
