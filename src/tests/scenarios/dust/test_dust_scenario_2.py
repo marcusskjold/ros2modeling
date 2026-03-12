394d2999b322dce86f878b20a345bf73fc673871
Andreas Riget Bagge <anrb@itu.dk>
2026-03-12 16:51:09 +0100

    dust scenario 1 indiv EXV1
    the first Dust scenario with executor V1 has been made, and gets same results as paper. Also name of assumption has been changed from A2 to A1, corresponding to paper.

--- src/tests/scenarios/dust/test_dust_scenario_2.py
+++ src/tests/scenarios/dust/test_dust_scenario_2.py
@@ -9,7 +9,7 @@ import roserer.adapters.dust_adapter as da
 
 from dotenv import load_dotenv
 load_dotenv()
-def test_dust_scenario_2_individual_A2_max_latency() -> None:
+def test_dust_scenario_2_individual_A1_max_latency() -> None:
     ros_system = yparse.parse_yaml("src/tests/input/dust_scenario_2_individual.yaml")
     test_result: sv.ValidationResult = sv.validate_system(ros_system)
     if test_result.errors != []:
@@ -39,3 +39,34 @@ def test_dust_scenario_2_individual_A2_max_latency() -> None:
                 'SUBSCRIBER1' : 13,
                 'SUBSCRIBER2' : 18
             }
+
+# def test_dust_scenario_2_individual_A2_max_latency() -> None:
+#     ros_system = yparse.parse_yaml("src/tests/input/dust_scenario_2_individual.yaml")
+#     test_result: sv.ValidationResult = sv.validate_system(ros_system)
+#     if test_result.errors != []:
+#         for ln in test_result.errors:
+#             print(ln)
+#             raise Exception("Something went wrong when validating the system!")
+#     else:
+#         errors, warnings, dust_system = da.transform_system(system=ros_system, validationresult=test_result)
+#         if errors != []:
+#             print(errors)
+#             raise Exception("Something went wrong transforming the system!")
+#         for ln in warnings:
+#             print(ln)
+#         if dust_system is not None:
+#             #print(dust_system.gen_declaration())
+#             #print(dust_system)
+#             #print(dust_system.gen_system())
+#             # uses state-space saving strategy.
+#             #print(dust_system.max_latency(stop_time=90))
+#             latency_results = dust_system.max_latency(prioritized=False, stop_time=90)
+#             assert latency_results == {
+#                 'TIMER0' : 22,
+#                 'SERVICE0' : 20,
+#                 'SERVICE1' : 25,
+#                 'SERVICE2' : 30,
+#                 'SUBSCRIBER0' : 12,
+#                 'SUBSCRIBER1' : 13,
+#                 'SUBSCRIBER2' : 18
+#             }
