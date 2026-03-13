48f711c560abc08487c65d7463bd96767f88fd8c
Andreas Riget Bagge <anrb@itu.dk>
2026-03-13 15:47:53 +0100

    dust scenario 1 holistic exv2
    scenario for dust scenario 1 holistic exv2 made and results same as paper. Also performs similarly with regards to performance

--- src/tests/scenarios/dust/test_dust_scenario_1.py
+++ src/tests/scenarios/dust/test_dust_scenario_1.py
@@ -36,18 +36,17 @@ def test_dust_scenario_1_individual_EXV2_A1_max_latency() -> None:
             #print(dust_system.gen_system())
             latency_results = dust_system.max_latency()
             assert latency_results == {
-                'TIMER0' : 28,
-                'TIMER1' : 33,
-                'TIMER2' : 17,
-                'TIMER3' : 22,
-                'SERVICE0' : 65,
-                'SERVICE1' : 70,
-                'SERVICE2' : 75,
-                'SUBSCRIBER0' : 65,
-                'SUBSCRIBER1' : 55,
-                'SUBSCRIBER2' : 60
+                'TIMER0_EX_0' : 28,
+                'TIMER1_EX_0' : 33,
+                'TIMER2_EX_0' : 17,
+                'TIMER3_EX_0' : 22,
+                'SERVICE0_EX_0' : 65,
+                'SERVICE1_EX_0' : 70,
+                'SERVICE2_EX_0' : 75,
+                'SUBSCRIBER0_EX_0' : 65,
+                'SUBSCRIBER1_EX_0' : 55,
+                'SUBSCRIBER2_EX_0' : 60
             }
-            #print(dust_system.max_latency())
 
 def test_dust_scenario_1_individual_EXV1_A1_max_latency() -> None:
     """
@@ -70,30 +69,27 @@ def test_dust_scenario_1_individual_EXV1_A1_max_latency() -> None:
         for ln in warnings:
             print(ln)
         if dust_system is not None:
-            #print(dust_system.gen_declaration())
-            #print(dust_system)
-            #print(dust_system.gen_system())
             latency_results = dust_system.max_latency()
             assert latency_results == {
-                'TIMER0' : 8,
-                'TIMER1' : 13,
-                'TIMER2' : 7,
-                'TIMER3' : 12,
-                'SERVICE0' : 65,
-                'SERVICE1' : 70,
-                'SERVICE2' : 75,
-                'SUBSCRIBER0' : 65,
-                'SUBSCRIBER1' : 55,
-                'SUBSCRIBER2' : 60
+                'TIMER0_EX_0' : 8,
+                'TIMER1_EX_0' : 13,
+                'TIMER2_EX_0' : 7,
+                'TIMER3_EX_0' : 12,
+                'SERVICE0_EX_0' : 65,
+                'SERVICE1_EX_0' : 70,
+                'SERVICE2_EX_0' : 75,
+                'SUBSCRIBER0_EX_0' : 65,
+                'SUBSCRIBER1_EX_0' : 55,
+                'SUBSCRIBER2_EX_0' : 60
             }
-            #print(dust_system.max_latency())
 
+#TODO: test
 def test_dust_scenario_1_holistic_EXV1_A1_max_latency() -> None:
     """
     Uses version 1 of the Executor. 
     Assumes all callbacks with release-time 0 is released
     before executor starts polling (assumption A1). 
-    Uses individual approach.
+    Uses holistic approach.
     """
     ros_system = yparse.parse_yaml("src/tests/input/dust_scenario_1_EXV1_holistic.yaml")
     test_result: sv.ValidationResult = sv.validate_system(ros_system)
@@ -109,20 +105,54 @@ def test_dust_scenario_1_holistic_EXV1_A1_max_latency() -> None:
         for ln in warnings:
             print(ln)
         if dust_system is not None:
-            #print(dust_system.gen_declaration())
-            #print(dust_system)
-            #print(dust_system.gen_system())
-            latency_results = dust_system.max_latency()
-            assert latency_results == {
-                'TIMER0' : 8,
-                'TIMER1' : 13,
-                'TIMER2' : 7,
-                'TIMER3' : 12,
-                'SERVICE0' : 65,
-                'SERVICE1' : 70,
-                'SERVICE2' : 75,
-                'SUBSCRIBER0' : 65,
-                'SUBSCRIBER1' : 55,
-                'SUBSCRIBER2' : 60
+            expected_result = {
+                'TIMER0_EX_1'      : 8,
+                'TIMER1_EX_1'      : 13,
+                'TIMER2_EX_1'      : 7,
+                'TIMER3_EX_1'      : 12,
+                'SUBSCRIBER0_EX_1' : 65,
+                'SUBSCRIBER1_EX_1' : 55,
+                'SUBSCRIBER2_EX_1' : 60,
+                'SERVICE0_EX_1'    : 65,
+                'SERVICE1_EX_1'    : 70,
+                'SERVICE2_EX_1'    : 75
             }
-            #print(dust_system.max_latency())
+            latency_results = dust_system.max_latency()
+            assert expected_result.items() <= latency_results.items()
+
+def test_dust_scenario_1_holistic_EXV2_A1_max_latency() -> None:
+    """
+    Uses version 2 of the Executor. 
+    Assumes all callbacks with release-time 0 is released
+    before executor starts polling (assumption A1). 
+    Uses holistic approach.
+    """
+    ros_system = yparse.parse_yaml("src/tests/input/dust_scenario_1_EXV2_holistic.yaml")
+    test_result: sv.ValidationResult = sv.validate_system(ros_system)
+    if test_result.errors != []:
+        for ln in test_result.errors:
+            print(ln)
+            raise Exception("Something went wrong when validating the system!")
+    else:
+        errors, warnings, dust_system = da.transform_system(system=ros_system, validationresult=test_result)
+        if errors != []:
+            print(errors)
+            raise Exception("Something went wrong transforming the system!")
+        for ln in warnings:
+            print(ln)
+        if dust_system is not None:
+            expected_result = {
+                'TIMER0_EX_1' : 28,
+                'TIMER1_EX_1' : 33,
+                'TIMER2_EX_1' : 17,
+                'TIMER3_EX_1' : 22,
+                'SERVICE0_EX_1' : 65,
+                'SERVICE1_EX_1' : 70,
+                'SERVICE2_EX_1' : 75,
+                'SUBSCRIBER0_EX_1' : 65,
+                'SUBSCRIBER1_EX_1' : 55,
+                'SUBSCRIBER2_EX_1' : 60
+            }
+            latency_results = dust_system.max_latency()
+            print(latency_results)
+            assert expected_result.items() <= latency_results.items()
