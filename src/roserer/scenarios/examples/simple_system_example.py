ed6a71304f4cedf4a889af9992ad3f892ce5cea3
Marcus Skjold <marcusskjold@gmail.com>
2026-03-26 13:22:25 +0100

    Fix simple system example

--- src/roserer/scenarios/examples/simple_system_example.py
+++ src/roserer/scenarios/examples/simple_system_example.py
@@ -14,23 +14,23 @@ executor = host.add_executor()
 # Node 1
 node_1 = executor.add_node(name="node_1")
 pub_1 = node_1.add_publisher(topic="topic_1", name="pub_1")
-timer_callback = node_1.add_callback(wcet=1, name="cb_1", publishers=[pub_1])
-timer_1 = node_1.add_timer(period=5, callback=timer_callback)
+timer_callback = node_1.add_callback(wcet=1, name="timer_1_callback", publishers=[pub_1])
+timer_1 = node_1.add_timer(period=5, callback=timer_callback, name="timer_1")
 
 # Node 2
 node_2 = executor.add_node(name="node_2")
 x = node_2.add_variable(name="x")
-subscription_callback = node_2.add_callback(wcet=1,write_variables=[x])
-subscription_1 = node_2.add_subscription(name="sub_1", callback=subscription_callback, topic="topic_1")
-service_callback = node_2.add_callback(wcet=1, name="serv_cb", read_variables=[x])
+subscription_callback = node_2.add_callback(wcet=1,write_variables=[x], name="subscription_callback")
+subscription_1 = node_2.add_subscription(name="subscription_1", callback=subscription_callback, topic="topic_1")
+service_callback = node_2.add_callback(wcet=1, name="service_callback", read_variables=[x])
 server_1 = node_2.add_service(name="service_1", callback=service_callback)
 
 # Node 3
 node_3 = executor.add_node(name="node_3")
 y = node_3.add_variable(name="y")
 client_1 = node_3.add_client(name="client_1", service="service_1")
-timer_2_callback = node_3.add_callback(wcet=1, name="timer_2_callback", request=ros.Request(client="client_1", response="response_cb"))
-response_cb = node_3.add_callback(wcet=1, name="response_cb", write_variables=[y])
+timer_2_callback = node_3.add_callback(wcet=1, name="timer_2_callback", request=ros.Request(client="client_1", response="response_callback"))
+response_cb = node_3.add_callback(wcet=1, name="response_callback", write_variables=[y])
 timer_2 = node_3.add_timer(period=10, callback=timer_2_callback, name="timer_2")
 
 feedback = sv.validate_system(system)
@@ -40,6 +40,6 @@ if feedback.errors != []:
 
 graph = RosGraphView(system)
 # graph_overview = graph.get_contracted_view().get_all_nodes()
-gd = GraphDrawer(graph, [NodeType.NODE, NodeType.CALLBACK, NodeType.TIMER, NodeType.SUBSCRIBER, NodeType.PUBLISHER, NodeType.SERVICE, NodeType.EXECUTOR, NodeType.HOST, NodeType.SYSTEM, NodeType.CLIENT])
-gd.save_to_file("results/simple_system_graph.svg")
+gd = GraphDrawer(graph, [NodeType.NODE, NodeType.CALLBACK, NodeType.VARIABLE, NodeType.TIMER, NodeType.SUBSCRIBER, NodeType.PUBLISHER, NodeType.SERVICE, NodeType.CLIENT])
+gd.save_to_file("results/simple_system_graph.pdf")
 #gp.transform_and_save_cb_graph(graph_overview, f"results/simple_system_graph.svg")
